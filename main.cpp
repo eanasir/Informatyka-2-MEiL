27f06dea096740bf11c7c3cfdac6eec5253d87e1
eanasir <mkkurkowicz@gmail.com>
2024-03-17 19:41:08 +0100

    poprawa ze względu na słowa prowadzącego - program dostał dwa punkty

--- main.cpp
+++ main.cpp
@@ -12,12 +12,12 @@ double ca³kaAnalityczna_2(double a, double b);
 
 int main() {
 	FILE* f, * g;
-	f = fopen("plik1.csv", "w");
+	f = fopen("plik1.txt", "w");
 	g = fopen("plik2.csv", "w");
-	double a = 1; //górna granica ca³kowania
-	double b = 5; //dolna granica ca³kowania
+	double a = 0; //górna granica ca³kowania
+	double b = 4*atan(1); //dolna granica ca³kowania
 	int n = 10; //liczba punktow dziel¹cych przedzia³ ca³kowania
-	int m = 4; //najwy¿sza potêga dwójki w ci¹gu u¿ywanym w pêtli
+	int m = 16; //najwy¿sza potêga dwójki w ci¹gu u¿ywanym w pêtli
 	double (*wskaznikFunkcja1)(double);//deklaracja wskaŸnika do pierwszej funkcji
 	double (*wskaznikFunkcja2)(double);
 	wskaznikFunkcja1 = &mojaFunkcja1;
@@ -25,14 +25,14 @@ int main() {
 	double bladCalkowania1 = fabs(ca³kaAnalityczna_1(a, b) - trapez(a, b, wskaznikFunkcja1, n));
 	for (int i = 1; i <= m; i++) {
 		int nCiag = pow(2, i);// n = 2,4,8,...,2^m
-		fprintf(f, "%lf,%lf,%d\n", ca³kaAnalityczna_1(a, b), trapez(a, b, wskaznikFunkcja1, nCiag), nCiag);
-		fprintf(g, "%lf,%lf,%d\n", ca³kaAnalityczna_1(a, b),simpson(a, b, wskaznikFunkcja1, nCiag), nCiag);
+		fprintf(f, "%d %4.16lf %4.16lf %4.16lf %4.16lf\n", nCiag, trapez(a, b, wskaznikFunkcja1, nCiag), fabs(2 - trapez(a, b, wskaznikFunkcja1, nCiag)), simpson(a, b, wskaznikFunkcja1, nCiag), fabs(2 - simpson(a, b, wskaznikFunkcja1, nCiag)));
+		fprintf(g, "%4.16lf,%4.16lf,%d\n", ca³kaAnalityczna_1(a, b),simpson(a, b, wskaznikFunkcja1, nCiag), nCiag);
 	}
 
 }
 
 double mojaFunkcja1(double x) {
-	return 1.0 / (x * x);
+	return sin(x);
 }
 double mojaFunkcja2(double x) {
 	return 1.0/ (x);
@@ -43,4 +43,8 @@ double ca³kaAnalityczna_1(double a, double b) {
 double ca³kaAnalityczna_2(double a, double b) {
 	return (log(a) - log(b));
 }
+double gauss(double (*pf)(double)) {
+	double xi[] = { -0.9061798459386639927976269, -0.5384693101056830910363144, 0.0,0.5384693101056830910363144 ,0.9061798459386639927976269 };
+	double wi[] = { 0.2369268850561890875142640 , 0.4786286704993664680412915 ,0.5688888888888888888888889 ,0.478628670499366468041291 ,0.2369268850561890875142640};
 
+}
